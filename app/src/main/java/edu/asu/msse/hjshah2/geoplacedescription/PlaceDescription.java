520437ec5bda6dbfde761c87c72986ec1be1c162
Himani0313 <hjshah2@asu.edu>
2017-02-04 14:57:03 -0700

    generating JSON string

--- app/src/main/java/edu/asu/msse/hjshah2/geoplacedescription/PlaceDescription.java
+++ app/src/main/java/edu/asu/msse/hjshah2/geoplacedescription/PlaceDescription.java
@@ -38,10 +38,10 @@ public class PlaceDescription {
     double latitude;
     double longitude;
 
-    PlaceDescription(String s){
+    PlaceDescription(String jsonStr, String name){
         try{
-            JSONObject jo = new JSONObject(s);
-            name = jo.getString("name");
+            JSONObject jo = new JSONObject(jsonStr);
+            this.name = name;
             description = jo.getString("description");
             category = jo.getString("category");
             addresstitle = jo.getString("addresstitle");
@@ -73,5 +73,53 @@ public class PlaceDescription {
                     "error converting to/from json");
         }
         return ret;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name =name;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description =description;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public void setCategory(String category){
+        this.category =category;
+    }
+    public String getAddresstitle(){
+        return addresstitle;
+    }
+    public void setAddresstitle(String addresstitle){
+        this.addresstitle =addresstitle;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String address){
+        this.address =address;
+    }
+    public double getElevation(){
+        return elevation;
+    }
+    public void setElevation(double elevation){
+        this.elevation =elevation;
+    }
+    public double getLatitude(){
+        return latitude;
+    }
+    public void setLatitude(double latitude){
+        this.latitude =latitude;
+    }
+    public double getLongitude(){
+        return longitude;
+    }
+    public void setLongitude(double longitude){
+        this.longitude =longitude;
     }
 }
--- app/src/main/java/edu/asu/msse/hjshah2/geoplacedescription/PlaceDescriptionLibrary.java
+++ app/src/main/java/edu/asu/msse/hjshah2/geoplacedescription/PlaceDescriptionLibrary.java
@@ -1,7 +1,73 @@
 package edu.asu.msse.hjshah2.geoplacedescription;
 
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by hjshah2 on 1/29/17.
  */
-public class PlaceDescriptionLibrary {
+public class PlaceDescriptionLibrary implements Serializable {
+    protected Hashtable<String, PlaceDescription> places;
+    private static PlaceDescriptionLibrary instance;
+    private static final boolean debugOn = true;
+
+    private PlaceDescriptionLibrary() {
+        places = new Hashtable<String, PlaceDescription>();
+    }
+
+    public synchronized static PlaceDescriptionLibrary initInstance(Context appContext) {
+        if(instance == null) {
+            instance = new PlaceDescriptionLibrary();
+            instance.loadPlaces(appContext);
+        }
+        return instance;
+    }
+
+    public static PlaceDescriptionLibrary getInstance() {
+        return instance;
+    }
+
+    private void debug(String msg) {
+        if(debugOn)
+            Log.d(this.getClass().getSimpleName(), msg);
+    }
+
+    public void loadPlaces(Context appContext) {
+        try {
+            places.clear();
+            String fileName = "places.json";
+            InputStream is = appContext.getResources().openRawResource(R.raw.places);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            JSONObject placesJSON = new JSONObject(new JSONTokener(br.readLine()));
+            Iterator<String> it = placesJSON.keys();
+            while(it.hasNext()) {
+                String pTitle = it.next();
+                JSONObject aPlace = placesJSON.optJSONObject(pTitle);
+                debug("importing place description titled " + pTitle + " json is " +aPlace.toString());
+                if(aPlace != null) {
+                    PlaceDescription md = new PlaceDescription(aPlace.toString(), pTitle);
+                    places.put(pTitle, md);
+                }
+            }
+        } catch(Exception e) {
+            Log.d(this.getClass().getSimpleName(), "Exception while loading places " + e.getMessage());
+        }
+    }
+    public PlaceDescription getPlaceDescription(String pTitle) {
+        return places.get(pTitle);
+    }
+
 }
